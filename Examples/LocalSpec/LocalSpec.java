985adebb5f5aee62ff913075750e03ea85686db3
Aaron Tomb <atomb@galois.com>
2011-10-11 15:38:04 -0700

    Wibbles to local spec example.

--- Examples/LocalSpec/LocalSpec.java
+++ Examples/LocalSpec/LocalSpec.java
@@ -9,12 +9,12 @@ class LocalSpec {
     public static void test2(int[] a) {
         a[0] = 1;
         // ensures a == 1;
-        a[0] = 2;
+        a[0]++;
         // ensures a == 2;
     }
 
     public static void test3(int[] a, int[] b) {
-        for(a[0] = 0; a[0] < b[0]; a[0]++) {
+        for(a[0] = 0; a[0] < 10; a[0]++) {
         }
     }
 
