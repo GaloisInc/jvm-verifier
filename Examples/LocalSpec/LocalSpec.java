55b69778688ce34657e8d75dda25d298a8fd7fdf
Aaron Tomb <atomb@galois.com>
2011-10-18 08:34:59 -0700

    Tweak LocalSpec example.

--- Examples/LocalSpec/LocalSpec.java
+++ Examples/LocalSpec/LocalSpec.java
@@ -14,7 +14,7 @@ class LocalSpec {
     }
 
     public static void test3(int[] a, int[] b) {
-        for(a[0] = 0; a[0] < b[0]; a[0]++) {
+        for(a[0] = 0; /* arbitrary: a */ a[0] < b[0]; a[0]++) {
         }
     }
 
