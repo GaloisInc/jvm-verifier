a8a3eb4bb2e2b21dedd4afd3e81d7f65cef3e925
Aaron Tomb <atomb@galois.com>
2011-11-14 10:55:24 -0800

    Breakpoints before instructions again.
    Reverts 083c49505c02a94d8074a1ae01c678b431244f6d.

--- sawscript/SAWScript/MethodSpec.hs
+++ sawscript/SAWScript/MethodSpec.hs
@@ -502,9 +502,7 @@ runMethod ir sec = do
     else do
       let Just thisRef = jecThis jec
       JSS.invokeInstanceMethod clName (methodKey method) thisRef args
-  when (pc /= 0) $ do
-    let pc' = nextPc method pc
-    Sem.setPc pc'
+  Sem.setPc pc
   -- Update the starting state with any 'ensures' located at the
   -- starting PC.
   --
