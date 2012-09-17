314a9d9c68153a8e4c1fb1889f13a4ae875bea40
Aaron Tomb <atomb@galois.com>
2012-09-17 12:58:13 -0700

    Fix ECC proof batch script.
    Now runs on Windows XP with only the JDK and Yices installed. Some
    issues remain when invoking Yices.

--- Examples/ECC/verify.bat
+++ Examples/ECC/verify.bat
@@ -1,13 +1,8 @@
-rmdir /s build
+rmdir /s /q build
 mkdir build
 javac -g -cp src;..\..\support src\com\galois\ecc\*.java -d build
 javac -g -cp build;tests tests\com\galois\ecc\*.java -d build
 cd saw
-cd spec
-cd sbv
-make_sbvs.bat
-cd ..
-cd ..
 cd proofs
-..\..\..\..\dist\build\sawScript\sawScript -c ..\..\build -j..\..\..\..\jdk1.6\classes.jar +RTS -K64M -RTS toplevel.saw
-cd ..
+..\..\..\..\bin\sawScript.exe -c ..\..\build -j"C:\Program Files\Java\jre7\lib\rt.jar" +RTS -K64M -RTS toplevel.saw
+cd ..\..
