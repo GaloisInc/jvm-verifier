4b75750411471044dbd257cc9fbc87fee4d3e200
Adam C. Foltzer <acfoltzer@galois.com>
2013-02-21 17:47:57 -0800

    progress converting the tests executable

--- test/src/Tests/ExpectedErrors.hs
+++ test/src/Tests/ExpectedErrors.hs
@@ -6,6 +6,8 @@ module Tests.ExpectedErrors (expErrTests) where
 
 import Control.Applicative
 import qualified Control.Exception as CE
+import Control.Lens
+import Control.Monad.Error
 import Control.Monad.State
 import Data.Typeable
 import System.IO
@@ -36,30 +38,24 @@ verb :: Int
 verb = 0
 
 go :: Codebase
-   -> Simulator SymbolicMonad a
+   -> Simulator SymbolicMonad IO a
    -> PropertyM IO ()
 go cb act = do
   -- For negative test cases, we don't want to report success of *any* failure,
   -- just the failures that we want to see, so we explicitly fail if any
   -- unexpected exception escapes out of m.
-  eea <- run $ do
-   oc <- mkOpCache
-   CE.try $ withSymbolicMonadState oc $ \sms ->  do
-     let sbe = symbolicBackend sms 
-     _ <- runDefSimulator sbe cb $ withVerbosity verb $ act
-     return [False]
-  case eea of
-    Left (e :: CE.SomeException) ->
-      let h :: (Show t, Typeable t, t ~ MonadTerm SymbolicMonad) =>
-               Maybe (SimulatorExc t)
-          h = CE.fromException e
-       in case h of
-            Just (SimExtErr msg v _) -> succeed msg v
-            _ -> emitErr e
-    Right chks -> assert (all not chks)
+  b <- run $ do 
+    oc <- mkOpCache
+    withSymbolicMonadState oc $ \sms -> do
+      let sbe = symbolicBackend sms
+          act' = (act >> return True) `catchError` h
+          h (ErrorPathExc (FailRsn rsn) _) = succeed rsn
+          h (UnknownExc (Just (FailRsn rsn))) = succeed rsn
+          h _ = succeed "unknown exception"
+      runDefSimulator cb sbe $ withVerbosity verb $ act'
+  assert b
   where
-    succeed msg v | v > 0     = run (putStrLn msg) >> assert False
-                  | otherwise = assert False
+    succeed msg = liftIO (putStrLn msg) >> return False
     emitErr e = do
       run $ mapM_ (hPutStrLn stderr)
               [ "vvv Test witnessed unexpected exception vvv"
@@ -73,14 +69,14 @@ go cb act = do
 
 -- | Negative test case: fail on single path exception
 exc1 :: TrivialProp
-exc1 cb = go cb $ runStaticMethod_ "Trivial" "always_throws" "()V" []
+exc1 cb = go cb . void $ runStaticMethod "Trivial" "always_throws" "()V" []
 
 -- | Negative test case: expect fail when all paths raise an exception
 exc2 :: TrivialProp
 exc2 cb = go cb $ do
-  sbe <- gets backend
+  sbe <- use backend
   b <- liftIO $ freshInt sbe
-  runStaticMethod_ "Errors" "allPathsThrowExc" "(Z)V" [IValue b]
+  void $ runStaticMethod "Errors" "allPathsThrowExc" "(Z)V" [IValue b]
 
 --------------------------------------------------------------------------------
 -- Array (negative) tests
@@ -88,14 +84,13 @@ exc2 cb = go cb $ do
 -- -- | Expected to fail: Symbolic lookup in array of refs
 sa1 :: TrivialProp
 sa1 cb = go cb $ do
-  sbe <- gets backend
+  sbe <- use backend
   symIdx <- liftIO $ IValue <$> freshInt sbe
   arr <- newMultiArray (ArrayType intArrayTy) [mkCInt 32 1, mkCInt 32 1]
-  [(pd, ReturnVal (RValue r))] <-
-    withoutExceptions
-    <$> runStaticMethod "Errors" "getArrayRef" "(I[[I)[I"
-          [symIdx , RValue arr]
-  getIntArray pd r
+  [(_, Just (RValue r))] <-
+    runStaticMethod "Errors" "getArrayRef" "(I[[I)[I"
+      [symIdx , RValue arr]
+  getIntArray r
 
 -- | Expected to fail: arrays with given element type are not supported
 sa2 :: Type -> TrivialProp
@@ -104,21 +99,20 @@ sa2 ety cb = go cb $ newMultiArray (ArrayType ety) [mkCInt 32 1]
 -- | Expected to fail: arrays with symbolic size are not supported
 sa3 :: TrivialProp
 sa3 cb = go cb $ do
-  sbe <- gets backend
+  sbe <- use backend
   v <- liftIO $ freshInt sbe
   newMultiArray intArrayTy [v]
 
 -- | Expected to fail: update an array of refs at a symbolic index
 sa4 :: TrivialProp
 sa4 cb = go cb $ do
-  sbe <- gets backend
+  sbe <- use backend
   symIdx <- liftIO $ IValue <$> freshInt sbe
   arr <- newMultiArray (ArrayType intArrayTy) [mkCInt 32 1, mkCInt 32 1]
   elm <- newIntArray intArrayTy [mkCInt 32 1]
-  [(_pd, Terminated)] <-
-    withoutExceptions
-    <$> runStaticMethod "Errors" "updArrayRef" "(I[I[[I)V"
-          [symIdx, RValue elm, RValue arr]
+  [(_pd, Nothing)] <-
+    runStaticMethod "Errors" "updArrayRef" "(I[I[[I)V"
+      [symIdx, RValue elm, RValue arr]
   return ()
 
 --------------------------------------------------------------------------------
