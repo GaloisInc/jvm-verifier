d8e3c8a2600495d45e997f0cdf77ca1fa1c4020f
Aaron Tomb <aarontomb@gmail.com>
2013-04-03 18:55:58 -0700

    Updates to JSS tutorial.

--- doc/japi-tutorial/code/ExampleCNF.java
+++ doc/japi-tutorial/code/ExampleCNF.java
@@ -1,7 +1,7 @@
 import com.galois.symbolic.*;
-class TestSat {
+class ExampleCNF {
     public static void main(String[] args) {
         byte x = Symbolic.freshByte((byte)0);
-        Symbolic.writeCnf("TestSat.cnf", x + x == 2 * x);
+        Symbolic.writeCnf("ExampleCNF.cnf", x + x == 2 * x);
     }
 }
