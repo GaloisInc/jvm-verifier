1ce6249c335dc5c018936a8a428c90cb751fa3c3
Aaron Tomb <atomb@galois.com>
2013-04-12 13:37:12 -0700

    More updates to Jenkins scripts.

--- jenkins.sh
+++ jenkins.sh
@@ -1,11 +1,11 @@
 # Jenkins should have "cd Java && bash jenkins.sh"
 PATH="$HOME/.cabal/bin:$(eval 'echo $'"$HASKELL_RUNTIME"):$TOOLS:$JDK16:$PATH"
 
-cabal update
-cabal install cabal-dev
-cabal install alex
-cabal install happy
-cabal install c2hs
+#cabal update
+#cabal install cabal-dev
+#cabal install alex
+#cabal install happy
+#cabal install c2hs
 
 cabal --version
 cabal-dev --version
@@ -22,6 +22,6 @@ git clone ssh://builder@src.galois.com/jvm-parser ./build/jvm-parser
 # git clone ssh://builder@src.galois.com/Aiger ./build/Aiger
 # cabal-dev add-source ./build/abcBridge
 # cabal-dev add-source ./build/Aiger
-cabal-dev install . ../Verinf ./build/abcBridge ./build/jvm-parser --constraint=hashable==1.1.2.5 --flags='build-tests'
+cabal-dev install . ../Verinf ../SAWCore ./build/abcBridge ./build/jvm-parser --flags=build-tests
 ./cabal-dev/bin/Tests
 bash stage.sh
