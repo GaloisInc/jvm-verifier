8708e62626c99deeed58813bec8d3371b038b495
Aaron Tomb <atomb@galois.com>
2013-08-12 09:32:39 -0700

    Update build scripts for JSS and LSS.

--- jenkins.sh
+++ jenkins.sh
@@ -1,28 +1,5 @@
 set -e
-# Jenkins should have "cd Java && bash jenkins.sh"
-PATH="$HOME/.cabal/bin:$(eval 'echo $'"$HASKELL_RUNTIME"):$TOOLS:$JDK16:$PATH"
 
-#cabal update
-#cabal install cabal-dev
-#cabal install alex
-#cabal install happy
-#cabal install c2hs
+bash build.sh
 
-cabal --version
-cabal-dev --version
-ghc --version
-ghc-pkg --version
-alex --version
-happy --version
-which c2hs
-c2hs --version
-
-mkdir -p ./build
-git clone ssh://builder@src.galois.com/abcBridge ./build/abcBridge
-git clone ssh://builder@src.galois.com/jvm-parser ./build/jvm-parser
-# git clone ssh://builder@src.galois.com/Aiger ./build/Aiger
-# cabal-dev add-source ./build/abcBridge
-# cabal-dev add-source ./build/Aiger
-cabal-dev install . ../Verinf ../SAWCore ./build/abcBridge ./build/jvm-parser --flags=build-tests
-./cabal-dev/bin/Tests
 bash stage.sh
