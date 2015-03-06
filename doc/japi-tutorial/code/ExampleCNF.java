7504edc8e06e39649c69997422c5fcc24ae16954
Nathan Collins <conathan@galois.com>
2015-03-06 15:41:49 -0800

    Remove negation before CNF generation in JSS.
    And update the example. SAWScript does not negate before generation,
    but JSS and LSS do. It's less surprising to be consistent, and it's
    easy enough to add a negation with '!' in the Java sources.

--- doc/japi-tutorial/code/ExampleCNF.java
+++ doc/japi-tutorial/code/ExampleCNF.java
@@ -2,6 +2,8 @@ import com.galois.symbolic.*;
 class ExampleCNF {
     public static void main(String[] args) {
         byte x = Symbolic.freshByte((byte)0);
-        Symbolic.writeCnf("ExampleCNF.cnf", x + x == 2 * x);
+        // To prove an equality with SAT we check that its negation is
+        // unsatisfiable.
+        Symbolic.writeCnf("ExampleCNF.cnf", ! (x + x == 2 * x));
     }
 }
